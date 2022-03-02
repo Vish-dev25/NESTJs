b6f4bfdcd16fbc4b044fb0a7db7c426dadff4244
Vish-dev25 <98530797+Vish-dev25@users.noreply.github.com>
2022-03-03 01:45:33 +0530

    Delete Services directory

--- Services/all.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-@Injectable()
-export class EmpAllService {
-    
-    loggerservice(ModuleName, ActionName)
-    {
-        console.log("UserLog", ModuleName, ActionName)
-    }
-}
